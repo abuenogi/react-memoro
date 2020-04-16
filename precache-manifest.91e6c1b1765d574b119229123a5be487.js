7e74cef3569258311b00280f0b737a2d37389b26
Ana Bueno <abuenogi@gmail.com>
2020-04-16 11:12:48 +0200

    Updates

---
+++ precache-manifest.91e6c1b1765d574b119229123a5be487.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45944f30f453ea7745b61b98ed6e5f20",
+    "url": "/react-memoro/index.html"
+  },
+  {
+    "revision": "3a579031a7e333b50ae6",
+    "url": "/react-memoro/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5a3c2299ad5fe907f5f8",
+    "url": "/react-memoro/static/js/2.eecb0d5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-memoro/static/js/2.eecb0d5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a579031a7e333b50ae6",
+    "url": "/react-memoro/static/js/main.976229ec.chunk.js"
+  },
+  {
+    "revision": "d77fbdb9d3f9d6757e56",
+    "url": "/react-memoro/static/js/runtime-main.a8229975.js"
+  },
+  {
+    "revision": "9b77d99fddb02e433572c5d8e695ef54",
+    "url": "/react-memoro/static/media/logo1.9b77d99f.png"
+  }
+]);
